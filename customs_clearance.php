28e7a7f55eb3d2c974bc81f2504ffed181baac94
Nasla123 <110897103+Nasla123@users.noreply.github.com>
2022-08-10 15:09:32 +0400

    Update customs_clearance.php

--- customs_clearance.php
+++ customs_clearance.php
@@ -1,5 +1,5 @@
-<?php include 'base.php' ?>
-<?php startblock('main') ?>
+<?php include 'layout/header.php' ?>
+
 
 
 <div class="site-blocks-cover inner-page-cover overlay"
@@ -10,7 +10,7 @@
 
             <div class="col-md-8" data-aos="fade-up" data-aos-delay="400">
                 <h1 class="text-white font-weight-light text-uppercase font-weight-bold">Customs Clearance</h1>
-                <p class="breadcrumb-custom"><a href="index">Home</a>
+                <p class="breadcrumb-custom"><a href="index.php">Home</a>
                     <span class="mx-2">&gt;</span><span>Customs Clearance</span></p>
             </div>
         </div>
@@ -50,4 +50,4 @@
     </div>
 </div>
 
-<?php endblock() ?>
+<?php include 'layout/footer.php' ?>
