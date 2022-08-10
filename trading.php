21dae89eaad637dc6e0a08cb0be0956311737c93
Nasla123 <110897103+Nasla123@users.noreply.github.com>
2022-08-10 15:19:56 +0400

    Update trading.php

--- trading.php
+++ trading.php
@@ -1,5 +1,5 @@
-<?php include 'base.php' ?>
-<?php startblock('main') ?>
+<?php include 'layout/header.php' ?>
+
 
 
 <div class="site-blocks-cover inner-page-cover overlay"
@@ -10,7 +10,7 @@
 
             <div class="col-md-8" data-aos="fade-up" data-aos-delay="400">
                 <h1 class="text-white font-weight-light text-uppercase font-weight-bold">Trading & Warehousing</h1>
-                <p class="breadcrumb-custom"><a href="index">Home</a>
+                <p class="breadcrumb-custom"><a href="index.php">Home</a>
                     <span class="mx-2">&gt;</span><span>Trading & Warehousing</span></p>
             </div>
         </div>
@@ -45,4 +45,4 @@
     </div>
 </div>
 
-<?php endblock() ?>
+<?php include'footer.php' ?>
