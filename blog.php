6f573ce37d46094b7ee1d0cf75d6cc35f90c692f
Nasla123 <110897103+Nasla123@users.noreply.github.com>
2022-08-10 15:05:04 +0400

    Update blog.php

--- blog.php
+++ blog.php
@@ -1,5 +1,5 @@
-<?php include 'base.php' ?>
-<?php startblock('main') ?>
+<?php include 'layout/header.php' ?>
+
 
 <div class="site-blocks-cover inner-page-cover overlay"
      style="background-image: url( 'assets/images/hero_bg_1.jpg' );" data-aos="fade"
@@ -9,7 +9,7 @@
 
             <div class="col-md-8" data-aos="fade-up" data-aos-delay="400">
                 <h1 class="text-white font-weight-light text-uppercase font-weight-bold">Our Blog</h1>
-                <p class="breadcrumb-custom"><a href="index">Home</a> <span
+                <p class="breadcrumb-custom"><a href="index.php">Home</a> <span
                         class="mx-2">&gt;</span> <span>Blog</span></p>
             </div>
         </div>
@@ -96,4 +96,4 @@
     </div>
 </div>
 
-<?php endblock() ?>
+<?php include 'layout/footer.php' ?>
